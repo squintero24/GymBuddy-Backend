733d0bb7fb3d82d432c98b61ffb8b172b03087f4
Santiago <santiagoquija123@gmail.com>
2023-11-01 11:17:32 -0500

    Document get all

--- src/main/java/com/example/gymbuddy/controller/UsuarioController.java
+++ src/main/java/com/example/gymbuddy/controller/UsuarioController.java
@@ -1,11 +1,16 @@
 package com.example.gymbuddy.controller;
 
 import com.example.gymbuddy.dto.UsuarioDto;
+import com.example.gymbuddy.model.DocumentType;
+import com.example.gymbuddy.model.Roles;
+import com.example.gymbuddy.repository.IRolesRepository;
 import com.example.gymbuddy.service.interfaces.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/user")
 @CrossOrigin(origins = "*")
@@ -14,6 +19,9 @@ public class UsuarioController {
 
     @Autowired
     private IUsuarioService usuarioService;
+
+    @Autowired
+    private IRolesRepository rolesRepository;
 
     @PostMapping("/login")
     public ResponseEntity<UsuarioDto> iniciarSesion(
@@ -42,6 +50,20 @@ public class UsuarioController {
         var deletedPerson = this.usuarioService.eliminarUsuario(idUser);
 
         return (deletedPerson) ? new ResponseEntity<>(true, HttpStatus.OK) : new ResponseEntity<>(false, HttpStatus.EXPECTATION_FAILED);
+    }
+
+
+    @GetMapping("/roles/all")
+    public ResponseEntity<List<Roles>> findAllTypeDocument() {
+
+        var roles = this.rolesRepository.findAll();
+
+        if (roles.isEmpty()) {
+            return new ResponseEntity<>(roles, HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(roles, HttpStatus.OK);
+
     }
 
 
